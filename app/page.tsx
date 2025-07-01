229ec448bba9e6baed0fc442915cf66d77550021
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-01 11:36:59 -0400

    used chess wrapper in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+
 import { ThemeToggle } from "@/components/theme-toggle";
-import { ChessGame } from "./components/chess-game";
+import { ChessWrapper } from "./components/chess-game/chess-wrapper";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="grid grid-cols-2 w-[90%]">
         <div className="flex items-center justify-center">
-          <ChessGame />
+          <ChessWrapper />
         </div>
         <div className="flex items-center justify-center">
           <ThemeToggle />
