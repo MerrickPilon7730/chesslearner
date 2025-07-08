bc10e6efaf033c790b403525ba1efa4ede0f31da
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-07 21:03:15 -0400

    Chnaged formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { ChessWrapper } from "./components/chess-game/chess-wrapper";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-5xl">
+    <div className="flex items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 w-[90%]">
         <div className="flex items-center justify-center py-10">
           <ChessWrapper />
         </div>
