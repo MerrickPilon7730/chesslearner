18c0b3dfeb7ba6987a970f636e79df1ee382a716
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-18 13:00:12 -0400

    Chenged color of header for light mode

--- components/header/header-wrapper.tsx
+++ components/header/header-wrapper.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "../theme-toggle";
 // Wrapper for header componenets
 export function HeaderWrapper() {
   return (
-    <div className="w-full bg-black py-8">
+    <div className="w-full dark:bg-black bg-blue-700 py-7">
       <div className="flex items-center justify-between max-w-screen-2xl mx-auto px-4">
         <div className="flex items-center">
           <HeaderLogo />
