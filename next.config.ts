0c96f1523644839efc68bd40914ff3921f161cda
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-19 16:10:05 -0400

    Updated to allow loading images from chessboardjs.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["chessboardjs.com"]
+  }
 };
 
 export default nextConfig;
