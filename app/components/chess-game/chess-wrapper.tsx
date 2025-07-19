e2d830c8a18cef55da508bff7b415ff43e88838f
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-19 13:41:52 -0400

    Added and used hook and components to display captured pieces

--- app/components/chess-game/chess-wrapper.tsx
+++ app/components/chess-game/chess-wrapper.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import { Chess } from "chess.js";
 
 import { Button } from "@/components/ui/button";
+
 import { ChessGame } from "./chess-game";
-import { Chess } from "chess.js";
-import { CapturedPieces } from "./captured-pieces";
+import { WhiteCapturedPieces } from "./white-captured-pieces";
+import { BlackCapturedPieces } from "./black-captured-pieces";
+import { useCapturedPieces } from "@/hooks/useCapturedPieces";
 
 type Props = {
   setMoveHistory: React.Dispatch<React.SetStateAction<string[][]>>;
@@ -14,76 +17,81 @@ type Props = {
 
 // This component acts as a wrapper for game state and UI controls
 export function ChessWrapper({setMoveHistory, moveHistory}: Props) {
-  // Tracks the player's side (white/black)
-  const [side, setSide] = useState<"black" | "white">("white");
-  // Formatted string for the button to switch sides
-  const [string, setString] = useState<"Black" | "White">("Black");
-  // Initializes a new chess game instance 
-  const [game, setGame] = useState(new Chess());
-  // Tracks if game is over, used for ending/resetting the game
-  const [isGameOver, setIsGameOver] = useState(false);
-  // Tracks the current difficulty/depth of Stockfish
-  const [difficulty, setDifficulty] = useState(5);
+	// Tracks the player's side (white/black)
+	const [side, setSide] = useState<"black" | "white">("white");
+	// Formatted string for the button to switch sides
+	const [string, setString] = useState<"Black" | "White">("Black");
+	// Initializes a new chess game instance 
+	const [game, setGame] = useState(new Chess());
+	// Tracks if game is over, used for ending/resetting the game
+	const [isGameOver, setIsGameOver] = useState(false);
+	// Tracks the current difficulty/depth of Stockfish
+	const [difficulty, setDifficulty] = useState(5);
+	// Converts move history from string[][] to string[]
+	const flatMoveHistory = useMemo(() => moveHistory.flat(), [moveHistory]);
+	// Uses flatMoveHistory to keep track of pieces captured
+	const {whiteCaptured, blackCaptured} = useCapturedPieces(flatMoveHistory);
 
-  // Called when player wants to switch sides. It switches sides and resets the game.
-  function onClick() {
-    setSide(side === "white" ? "black" : "white");
-    setString(string === "Black" ? "White" : "Black");
-    setGame(new Chess());
-    setIsGameOver(false);
-    setMoveHistory([]);
-  } 
+	// Called when player wants to switch sides. It switches sides and resets the game.
+	function onClick() {
+		setSide(side === "white" ? "black" : "white");
+		setString(string === "Black" ? "White" : "Black");
+		setGame(new Chess());
+		setIsGameOver(false);
+		setMoveHistory([]);
+	} 
 
-  // Resets the current game
-  function resetGame() {
-    setGame(new Chess());
-    setIsGameOver(false);
-    setMoveHistory([]);
-  }
+	// Resets the current game
+	function resetGame() {
+		setGame(new Chess());
+		setIsGameOver(false);
+		setMoveHistory([]);
+	}
 
-  return (
-    <div className="w-full flex flex-col justify-center items-center px-4">
-      <div className="w-full flex justify-center px-4 gap-4">
-        <Button variant="default" onClick={onClick}>
-          Play As {string}
-        </Button>
-        <Button variant="default" onClick={resetGame}>
-          Reset
-        </Button>
+	return (
+		<div className="w-full flex flex-col justify-center items-center px-4">
+			<div className="w-full flex justify-center px-4 gap-4">
+				<Button variant="default" onClick={onClick}>
+					Play As {string}
+				</Button>
+				<Button variant="default" onClick={resetGame}>
+					Reset
+				</Button>
 
-        <div className="flex items-center w-64 gap-2">
-          <label
-            htmlFor="difficulty"
-            className="text-md whitespace-nowrap "
-          >
-            Difficulty: {difficulty}
-          </label>
+				<div className="flex items-center w-64 gap-2">
+					<label
+						htmlFor="difficulty"
+						className="text-md whitespace-nowrap "
+					>
+						Difficulty: {difficulty}
+					</label>
 
-          <input
-            type="range"
-            id="difficulty"
-            min={1}
-            max={20}
-            value={difficulty}
-            onChange={(e) => setDifficulty(Number(e.target.value))}
-            className="flex-1"
-          />
-        </div>
-      </div>
-      
-      <div className="w-full flex justify-center mt-4">
-        <ChessGame
-          game={game}
-          setGame={setGame}
-          side={side}
-          isGameOver={isGameOver}
-          setIsGameOver={setIsGameOver}
-          difficulty={difficulty}
-          setMoveHistory={setMoveHistory}
-        />
-		<CapturedPieces moveHistory={moveHistory.flat()}/>
-      </div>
+					<input
+						type="range"
+						id="difficulty"
+						min={1}
+						max={20}
+						value={difficulty}
+						onChange={(e) => setDifficulty(Number(e.target.value))}
+						className="flex-1"
+					/>
+				</div>
+			</div>
+			
+			<div className="w-full flex justify-center mt-4">
+				<BlackCapturedPieces pieces={blackCaptured}/>
+				<ChessGame
+					game={game}
+					setGame={setGame}
+					side={side}
+					isGameOver={isGameOver}
+					setIsGameOver={setIsGameOver}
+					difficulty={difficulty}
+					setMoveHistory={setMoveHistory}
+					/>
+					<WhiteCapturedPieces pieces={whiteCaptured}/>
+			</div>
 
-    </div>
-  );
+		</div>
+	);
 }
