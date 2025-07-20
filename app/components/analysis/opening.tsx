23832956a50120a9d352efd4736a86e0522c6f45
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-20 19:34:38 -0400

    Used hook to show the played opening

--- app/components/analysis/opening.tsx
+++ app/components/analysis/opening.tsx
@@ -1,5 +1,18 @@
 
+import { useOpeningName } from "@/hooks/useOpeningName"
 
-export function Opening(){
-    
+type Props ={
+    moveHistory: string[];
+}
+
+export function Opening({moveHistory}: Props){
+    const openingName = useOpeningName(moveHistory);
+
+    return(
+        <div className="w-[90%] mb-10">
+            <p className="flex justify-center bg-blue-800 dark:bg-zinc-500 text-xl">
+                <b className="mr-2">Opening:</b>{openingName ?? ""}
+            </p>
+        </div>
+    )
 }
