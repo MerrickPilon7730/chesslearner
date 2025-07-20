db0171cdc93df2dc30ef75282ec42989aa8f7412
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-20 19:34:19 -0400

    Passed move history to opening component

--- app/components/analysis/analysis-wrapper.tsx
+++ app/components/analysis/analysis-wrapper.tsx
@@ -6,12 +6,14 @@ import {
     CardContent,
 } from "@/components/ui/card"
 import { MoveHistory } from "./move-history";
+import { Opening } from "./opening";
 
 type Props = {
     moveHistory: string[][];
 };
 
 export function AnalysisWrapper({ moveHistory }: Props) {
+    console.log("Flat moveHistory:", moveHistory.flat());
     return (
         <Card className="w-full h-full">
             <CardHeader className="flex items-center justify-center">
@@ -19,7 +21,8 @@ export function AnalysisWrapper({ moveHistory }: Props) {
                     Analysis
                 </CardTitle>
             </CardHeader>
-            <CardContent className="w-full flex justify-center items-start">
+            <CardContent className="w-full flex flex-col justify-center items-center">
+                <Opening moveHistory={moveHistory.flat()}/>
                 <MoveHistory moveHistory={moveHistory} />
             </CardContent>
         </Card>
