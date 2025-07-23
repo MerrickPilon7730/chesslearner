766d742cbc10933446ffb2fd8b48090836922355
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-23 15:14:34 -0400

    Switched div to card and changed layout

--- app/components/chess-game/modals/reset-game.tsx
+++ app/components/chess-game/modals/reset-game.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { 
+    Card,
+    CardTitle,
+    CardDescription,
+    CardContent,
+ } from "@/components/ui/card";
 
 // Properties for the component
 type Props = {
@@ -10,12 +16,15 @@ type Props = {
 export function ResetGame({onConfirm, onCancel}: Props) {
     return (
         <div className="absolute inset-0 flex items-center justify-center z-50">
-            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-h-[30%] min-w-[40%] max-w-[60%]">
-                <h2 className="text-2xl font-bold">
-                    Are you sure you want reset the game?
-                </h2>
+            <Card className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-h-[20%] min-w-[40%] max-w-[60%]">
+                <CardTitle className="text-xl font-bold -mb-1">
+                    Reset The Game?
+                </CardTitle>
+                <CardDescription className="text-white -my-2">
+                    This will start a new game.
+                </CardDescription>
 
-				<div className="flex gap-4 justify-center">
+				<CardContent className="flex gap-4 justify-center">
 					<Button onClick={onConfirm} className="px-4 py-2 rounded transition">
 						Yes
 					</Button>
@@ -24,8 +33,8 @@ export function ResetGame({onConfirm, onCancel}: Props) {
 							Cancel
 						</Button>
 					)}
-				</div>
-            </div>
+				</CardContent>
+            </Card>
         </div>
     )
 }
