c7b01736b956660f021b42d638f231c7ea8df300
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-23 09:45:54 -0400

    Adjusted min height and width

--- app/components/chess-game/chess-start.tsx
+++ app/components/chess-game/chess-start.tsx
@@ -14,9 +14,9 @@ type Props = {
 export function ChessStart({setGameStart, difficulty, setDifficulty}: Props) {
     return (
         <div className="absolute inset-0 flex items-center justify-center z-50">
-            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4">
+            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-w-[40%] min-h-[30%]">
                 <h2 className="text-2xl font-bold">
-                    Select a difficulty
+                    Select Difficulty
                 </h2>
 
                 <div className="flex flex-col items-center gap-2">
