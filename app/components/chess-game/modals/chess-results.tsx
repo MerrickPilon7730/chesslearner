7e4d7d4b870596908059de59ddb994f5a31700b4
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-23 12:36:03 -0400

    Set min height and width

--- app/components/chess-game/modals/chess-results.tsx
+++ app/components/chess-game/modals/chess-results.tsx
@@ -16,7 +16,7 @@ type Props = {
 export function ChessResults({winner, reset, difficulty, setDifficulty}: Props) {
     return (
         <div className="absolute inset-0 flex items-center justify-center z-50">
-            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4">
+            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-w-[40%] min-h-[30%]">
                 <h2 className="text-2xl font-bold">
                     {winner === "Draw" ? "It's a Draw!" : `${winner} Wins!`}
                 </h2>
