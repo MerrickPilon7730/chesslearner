85074b8b5c9f5c8e0cc6e73382a8195d0a9e4535
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-23 15:23:14 -0400

    Switched div to Card and changed layout

--- app/components/chess-game/modals/play-as.tsx
+++ app/components/chess-game/modals/play-as.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
 // Properties for the component
 type Props = {
@@ -6,26 +7,28 @@ type Props = {
     onCancel: () => void;
 }
 
-// Overlay/Modal that displays the winner and a reset/play again button
+// Overlay/Modal that confirms switching sides/color
 export function PlayAs({onConfirm, onCancel}: Props) {
     return (
         <div className="absolute inset-0 flex items-center justify-center z-50">
-            <div className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-h-[30%] min-w-[40%] max-w-[60%]">
-                <h2 className="text-2xl font-bold">
-                    Are you sure you want to switch sides? This will reset the game!
-                </h2>
+            <Card className="bg-blue-200 dark:bg-[#3D3D3E] p-6 rounded-lg shadow-xl text-center space-y-4 min-h-[20%] min-w-[40%] max-w-[60%]">
+                <CardTitle className="text-xl font-bold -mb-1">
+                    Switch Sides?
+                </CardTitle>
+                
+                <CardDescription className="text-white -my-2">
+                    This will start a new game.
+                </CardDescription>
 
-				<div className="flex gap-4 justify-center">
+				<CardContent className="flex gap-4 justify-center">
 					<Button onClick={onConfirm} className="px-4 py-2 rounded transition">
 						Yes
 					</Button>
-					{onCancel && (
-						<Button onClick={onCancel} className="px-4 py-2 rounded transition">
-							Cancel
-						</Button>
-					)}
-				</div>
-            </div>
+					<Button onClick={onCancel} className="px-4 py-2 rounded transition">
+						Cancel
+					</Button>
+				</CardContent>
+            </Card>
         </div>
     )
 }
