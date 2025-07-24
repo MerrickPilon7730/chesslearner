987c9c21ee1ede3cf84392d93c0631f03f626146
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-24 17:27:19 -0400

    Used navbar component

--- components/header/header-wrapper.tsx
+++ components/header/header-wrapper.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { HeaderLogo } from "./header-logo";
 import { ThemeToggle } from "../theme-toggle";
+import { Navbar } from "./navbar/navbar";
 
 // Wrapper for header componenets
 export function HeaderWrapper() {
   return (
     <div className="w-full dark:bg-black bg-blue-700 py-7">
       <div className="flex items-center justify-between max-w-screen-2xl mx-auto px-4">
-        <div className="flex items-center">
+        <div className="flex items-center gap-x-10">
           <HeaderLogo />
+          <Navbar />
         </div>
         <div className="text-white">
           <ThemeToggle />
