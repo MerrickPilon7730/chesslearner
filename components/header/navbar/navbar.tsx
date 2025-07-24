e92b470ea9fc12801c07dd5998908dce4314e212
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-24 17:26:52 -0400

    Created a navbar

---
+++ components/header/navbar/navbar.tsx
@@ -0,0 +1,31 @@
+
+import { usePathname } from "next/navigation";
+import { NavButton } from "./nav-button";
+
+const navLinks = [
+    {
+        name: "Play",
+        href: "/"
+    },
+    {
+        name: "Learn",
+        href: "/learn"
+    }
+]
+
+export function Navbar() {
+    const pathName = usePathname();
+
+    return(
+        <div className="flex items-center gap-x-2">
+            {navLinks.map((link) =>(
+                <NavButton 
+                    key={link.href}
+                    href={link.href}
+                    label={link.name}
+                    isActive={link.href === pathName}
+                />
+            ))}
+        </div>
+    )
+};
