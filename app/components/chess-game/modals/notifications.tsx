e88dcea7dbca29a228eb2c0c424de2d6f5ad617e
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-25 20:23:47 -0400

    Edited format of buttons and Card

--- app/components/chess-game/modals/notifications.tsx
+++ app/components/chess-game/modals/notifications.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import { WinnerInfo } from "../chess-game";
 
 type Props = {
@@ -27,44 +27,51 @@ export function ChessNotification({
 
 	return (
 		<div className="absolute inset-0 flex items-center justify-center z-50">
-			<Card className="p-6 rounded-lg shadow-xl text-center space-y-4 min-w-[40%] min-h-[20%]">
+			<Card className="p-6 rounded-lg shadow-xl text-center space-y-4 min-w-[50%] min-h-[20%]">
 				{winner && (
-				<p className="text-lg font-bold text-green-700">
+				<CardTitle className="text-2xl font-bold text-green-700 -mb-2">
 					{winner.result} wins by {winner.message}
-				</p>
+				</CardTitle>
 				)}
 
-				<CardTitle className="text-2xl font-bold">
+				<CardDescription className="text-xl font-bold text-white -mb-2">
 					Select Difficulty
-				</CardTitle>
+				</CardDescription>
 
-				<div className="flex flex-col items-center gap-2">
-					<label htmlFor="difficulty" className="text-md whitespace-nowrap">
-						Difficulty: {difficulty}
-					</label>
-					<input
-						type="range"
-						id="difficulty"
-						min={1}
-						max={20}
-						value={difficulty}
-						onChange={(e) => setDifficulty(Number(e.target.value))}
-						className="w-full"
-					/>
-				</div>
+				<CardContent>
+					<div className="flex flex-col items-center gap-2 mb-5">
+						<label htmlFor="difficulty" className="text-md whitespace-nowrap">
+							Difficulty: {difficulty}
+						</label>
+						<input
+							type="range"
+							id="difficulty"
+							min={1}
+							max={20}
+							value={difficulty}
+							onChange={(e) => setDifficulty(Number(e.target.value))}
+							className="w-full"
+						/>
+					</div>
 
-				<div className="flex flex-col gap-y-3 md:flex-row md:gap-x-3">
-					<Button variant="default" onClick={handleSwitchSides}>
-						Play As {oppositeSide}
-					</Button>
+					<div className="flex flex-col gap-y-3 md:flex-row md:gap-x-3">
+						<Button 
+							variant="default" 
+							onClick={handleSwitchSides}
+							className="min-w-[50%]"
+							>
+							Play As {oppositeSide}
+						</Button>
 
-					<Button
-						variant="default"
-						onClick={reset}
-					>
-						Start Game
-					</Button>
-				</div>
+						<Button
+							variant="default"
+							onClick={reset}
+							className="min-w-[50%]"
+						>
+							Start Game
+						</Button>
+					</div>
+				</CardContent>
 			</Card>
 		</div>
 	);
