f44714dd5cc030d259125e40757edd6114136c4d
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-27 20:42:05 -0400

    Updated layout to be similar to main page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,11 +1,23 @@
 
 "use client"
 
-export default function Learn() {
+import { useState } from "react";
 
-  return (
-    <div className="flex items-center justify-center">
-        Learn Page
-    </div>
-  );
+import { ChessLearnGame } from "./components/chess-learn-game";
+
+export default function Learn() {
+	const [moveHistory, setMoveHistory] = useState<string[][]>([]);
+
+	return (
+		<div className="flex items-center justify-center">
+			<div className="grid grid-cols-1 md:grid-cols-2 w-[90%]">
+				<div className="flex items-center justify-center py-5">
+					<ChessLearnGame setMoveHistory={setMoveHistory}/>
+				</div>
+				<div className="flex items-center justify-center py-5">
+					Analysis
+				</div>
+			</div>
+		</div>
+	);
 }
