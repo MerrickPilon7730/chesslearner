3404f2b65d68577f6474209bc857865ef07e4409
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-28 20:31:15 -0400

    Created fenHistory state and passed it to chess wrapper

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -5,15 +5,17 @@ import { useState } from "react";
 
 import { ChessLearnWrapper } from "./components/chess-learn-wrapper";
 import { AnalysisLearnWrapper } from "./components/analysis-learn-wrapper";
+import { MoveHistory } from "@/types/game";
 
 export default function Learn() {
-	const [moveHistory, setMoveHistory] = useState<string[][]>([]);
+	const [moveHistory, setMoveHistory] = useState<MoveHistory>([]);
+	const [fenHistory, setFenHistory] = useState<string[]>([]);
 
 	return (
 		<div className="flex items-center justify-center">
 			<div className="grid grid-cols-1 md:grid-cols-2 w-[90%]">
 				<div className="flex items-center justify-center py-5">
-					<ChessLearnWrapper moveHistory={moveHistory} setMoveHistory={setMoveHistory}/>
+					<ChessLearnWrapper moveHistory={moveHistory} setMoveHistory={setMoveHistory} setFenHistory={setFenHistory}/>
 				</div>
 				<div className="flex items-center justify-center py-5">
 					<AnalysisLearnWrapper moveHistory={moveHistory}/>
