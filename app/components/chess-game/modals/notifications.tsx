c7d834e5f9b07e7bfc0278ddb56d8c891c62b31b
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-07-30 15:28:27 -0400

    able to minimize notification

--- app/components/chess-game/modals/notifications.tsx
+++ app/components/chess-game/modals/notifications.tsx
@@ -9,6 +9,8 @@ import {
 	CardTitle 
 } from "@/components/ui/card";
 
+import { cn } from "@/lib/utils";
+
 import { 
 	Difficulty, 
 	SetDifficulty, 
@@ -41,72 +43,80 @@ export function ChessNotification({
 		setSide(side === "white" ? "black" : "white");
 	}
 
-	  return (
-    <div className="absolute inset-0 flex items-center justify-center z-50 p-4">
-      <Card className="rounded-lg shadow-xl w-full max-w-[60%] min-w-[40%] text-center">
-        <div className="flex items-center justify-between p-4 border-b border-gray-700 bg-gray-800 rounded-t-lg">
-          <CardTitle className="text-xl text-white">
-            {winner ? (
-              winner.result === "Draw" ? (
-                <p>{winner.result} by {winner.message}</p>
-              ) : (
-                <p>{winner.result} wins by {winner.message}</p>
-              )
-            ) : (
-              <p>Game Settings</p>
-            )}
-          </CardTitle>
-          <Button
-            variant="ghost"
-            size="sm"
-            onClick={() => setIsMinimized(!isMinimized)}
-            className="text-white"
-          >
-            {isMinimized ? "🔼" : "🔽"}
-          </Button>
-        </div>
+	return (
+		<div className="absolute inset-0 flex items-center justify-center z-50 p-4">
+			<Card className={cn(
+				"transition-all duration-500 z-50",
+				isMinimized
+					? "absolute bottom-0 right-0 max-w-[40%] max-h-[10%] text-sm"
+					: "rounded-lg shadow-xl w-full max-w-[60%] min-w-[40%] text-center")}>
+				<div className="flex items-center justify-center px-6">
+					<CardTitle className={cn(
+						"text-white",
+						isMinimized ? "text-sm flex items-center justify-center" : "text-xl"
+					)}>
+						{winner ? (
+							winner.result === "Draw" ? (
+								<p className="text-green-700">{winner.result} by {winner.message}</p>
+							) : (
+								<p className="text-green-700">{winner.result} wins by {winner.message}</p>
+							)
+							) : (
+								<p>Game Settings</p>
+							)
+						}
+					</CardTitle>
+					<Button
+						variant="ghost"
+						size="sm"
+						onClick={() => setIsMinimized(!isMinimized)}
+						className="text-white"
+					>
+						{isMinimized ? "🔼" : "🔽"}
+					</Button>
+				</div>
 
-        {!isMinimized && (
-          <CardContent className="space-y-4 bg-gray-900 text-white">
-            <CardDescription className="text-lg font-bold text-white">
-              Select Difficulty
-            </CardDescription>
+				{!isMinimized && (
+					<CardContent className="space-y-4 text-white">
+						<CardDescription className="text-lg font-bold text-white">
+							Select Difficulty
+						</CardDescription>
 
-            <div className="flex flex-col items-center gap-2 mb-5">
-              <label htmlFor="difficulty" className="text-md whitespace-nowrap">
-                Difficulty: {difficulty}
-              </label>
-              <input
-                type="range"
-                id="difficulty"
-                min={1}
-                max={20}
-                value={difficulty}
-                onChange={(e) => setDifficulty(Number(e.target.value))}
-                className="w-full"
-              />
-            </div>
+						<div className="flex flex-col items-center gap-2 mb-5">
+							<label htmlFor="difficulty" className="text-md whitespace-nowrap">
+								Difficulty: {difficulty}
+							</label>
+							<input
+								type="range"
+								id="difficulty"
+								min={1}
+								max={20}
+								value={difficulty}
+								onChange={(e) => setDifficulty(Number(e.target.value))}
+								className="w-full"
+							/>
+						</div>
 
-            <div className="flex flex-col gap-y-3 md:flex-row md:gap-x-3">
-              <Button
-                variant="default"
-                onClick={handleSwitchSides}
-                className="min-w-[50%]"
-              >
-                Play As {oppositeSide}
-              </Button>
+						<div className="flex flex-col gap-y-3 md:flex-row md:gap-x-3">
+							<Button
+								variant="default"
+								onClick={handleSwitchSides}
+								className="min-w-[50%]"
+							>
+								Play As {oppositeSide}
+							</Button>
 
-              <Button
-                variant="default"
-                onClick={reset}
-                className="min-w-[50%]"
-              >
-                Start Game
-              </Button>
-            </div>
-          </CardContent>
-        )}
-      </Card>
-    </div>
-  );
+							<Button
+								variant="default"
+								onClick={reset}
+								className="min-w-[50%]"
+							>
+								Start Game
+							</Button>
+						</div>
+					</CardContent>
+				)}
+			</Card>
+		</div>
+	);
 }
