05028f1f37cdb19a283fb0199f73573bd0e69743
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-08-05 11:31:39 -0400

    Added props

--- app/learn/components/analysis/evaluation-bar.tsx
+++ app/learn/components/analysis/evaluation-bar.tsx
@@ -1,6 +1,12 @@
+import { StockfishResponse } from "@/types/game"
 
+type Props ={
+    stockfishData: StockfishResponse
+}
 
-export function EvaluationBar()  {
+export function EvaluationBar({
+    stockfishData,
+}: Props)  {
     return (
         <div>
             Evaluation Bar
