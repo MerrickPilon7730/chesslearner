1c7046ba27ef3f00b1f4bb6c6f76ed493157f68c
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-08-07 19:03:55 -0400

    added an overflow to card content

--- app/learn/components/analysis-learn-wrapper.tsx
+++ app/learn/components/analysis-learn-wrapper.tsx
@@ -72,7 +72,7 @@ export function AnalysisLearnWrapper({
         <Card className="w-full h-full max-w-[90%] max-h-90%]">
             <CardHeader className="flex items-center justify-center">
             </CardHeader>
-            <CardContent className="w-full flex flex-col justify-center items-center">
+            <CardContent className="w-full flex flex-col items-center gap-y-4 overflow-y-auto max-h-[75vh] p-4">
                 <AIComponent analysis={aiResponse} />
                 <Opening moveHistory={moveHistory}/>
                 <MoveHistoryComp moveHistory={moveHistory} heightClamp="max-h-[200px]" />
