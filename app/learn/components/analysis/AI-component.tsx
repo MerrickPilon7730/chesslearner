341db536a4ceccef9510f37de601915010726f0e
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-08-08 09:39:30 -0400

    Used isThinking state to render a "skelleton"

--- app/learn/components/analysis/AI-component.tsx
+++ app/learn/components/analysis/AI-component.tsx
@@ -2,15 +2,28 @@
 
 type Props = {
     analysis: string;
+    isThinking: boolean;
 }
 
 export function AIComponent({
     analysis,
+    isThinking,
 }: Props)  {
 
+    if (isThinking){
+        return (
+            <div className="w-[90%] overflow-auto text-white max-h-[200px] min-h-[200px] flex flex-col">
+                <p className="text-2xl font-bold text-center mb-2">Analysis</p>
+
+                <div className="flex-1 flex items-center justify-center">
+                    <p className="italic">Thinking...</p>
+                </div>
+            </div>
+        )
+    }
 
     return (
-        <div className="w-[90%] overflow-auto text-white max-h-[200px]">
+        <div className="w-[90%] overflow-auto text-white max-h-[200px] min-h-[200px]">
             <p className="flex justify-center text-2xl font-bold mb-2">Analysis</p>
             <p>{analysis}</p>
         </div>
